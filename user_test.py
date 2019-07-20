43ffc77770e7f7012eaf3a1e1d4355c0679332d4
James Kirwa <jameskirwa34@gmail.com>
2019-07-21 01:04:43 +0300

    add a comment to user_test.py

--- user_test.py
+++ user_test.py
@@ -5,3 +5,9 @@ class TestUser(unittest.TestCase):
     """
     Class for testing the behaviour of the user class
     """
+
+    def setUp(self):
+        """
+        Setup method that defines instructions
+        """
+        self.new_user = user("James","Kirwa","Uppercase95")
