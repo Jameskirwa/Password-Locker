afd3637ef5e78c18714eca367baf2ea571b4521c
James Kirwa <jameskirwa34@gmail.com>
2019-07-20 22:04:02 +0300

    add function to save user logoins

--- password.py
+++ password.py
@@ -13,6 +13,17 @@ class user:
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
-        
+
+        # instance variables that take (firstname,lastname & password)
+    def save_user(self):
+        """
+        Function to save user login details
+        """
+        user.user_list.append(self)    
+
+     
+
+
+
 
     
