2ed47c9866371eabea7b27d7fcaccf1622556a66
James Kirwa <jameskirwa34@gmail.com>
2019-07-21 02:14:28 +0300

    import user/credentials

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from password import user
-from pasword import credentials
+from password import credentials
 
+def create_login(firstname,lastname,password):
+    """
+    Function to create a new login
+    """
+    new_user = user(firstname,lastname,password)
+    return new_user
