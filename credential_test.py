ce318c55e40dd097388da1c628ff9d104020f45a
James Kirwa <jameskirwa34@gmail.com>
2019-07-20 22:35:41 +0300

    add class credentials

---
+++ credential_test.py
@@ -0,0 +1,6 @@
+# import unittest
+# from password import Credentials
+# class TestCredentials(unittest.TestCase):
+#     """
+#     Class to test behaviour of the credentials class
+#     """
--- password.py
+++ password.py
@@ -13,13 +13,22 @@ class user:
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
-
-        # instance variables that take (firstname,lastname & password)
+        """
+        self is a var that reps the instance of an object itself
+        instance variables that take (firstname,lastname & password)
+        """
     def save_user(self):
         """
         Function to save user login details
         """
-        user.user_list.append(self)    
+        user.user_list.append(self) 
+
+class credentials:
+     """
+    Class to define credentials for the different accounts
+    """
+     credentials_list = []
+
 
      
 
