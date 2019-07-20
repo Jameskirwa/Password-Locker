e8bcaf8c6923477f59cd8e99142aac9de4d3f691
James Kirwa <jameskirwa34@gmail.com>
2019-07-20 21:32:01 +0300

    add Fuction for user initialization

--- password.py
+++ password.py
@@ -5,3 +5,10 @@ class user:
     Class to define login details for the user in the application
     """
     user_list = []
+
+    def __init__(self,first_name,last_name,password):
+        """
+        Function to initialize user fields correctly
+        """
+
+    
