6c0c45aadb29f3e3f93f98d25aa36793116b5f1d
James Kirwa <jameskirwa34@gmail.com>
2019-07-21 11:46:19 +0300

    add a setup method

--- credential_test.py
+++ credential_test.py
@@ -1,12 +1,14 @@
 import unittest  #importing the unitest module
 from password import credentials  #importing the credetial class
+
 class TestCredentials(unittest.TestCase):
     """
     Class to test behaviour of the credentials class
     """
-    # def setUp(self):
-    
-    # #Setup method that defines instructions
-    
-    # self.new_credentials = Credentials("Github","Kibet1816","@#soccerkibe1816")
+    def setUp(self):
+        """
+        Setup method that defines instructions
+        """
+        self.new_credentials = credentials("Github","Jameskirwa","Uppercase95")
+
     
